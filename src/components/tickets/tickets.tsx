12841b030bd4f2148656a0edc058f64d6507b739
Roman Prudnikov <stillerr@ya.ru>
2019-09-18 23:49:35 +0300

    Main filter styles

--- src/components/tickets/tickets.tsx
+++ src/components/tickets/tickets.tsx
@@ -5,15 +5,15 @@ import Ticket from '../ticket/ticket';
 const Tickets: React.FC = () => {
     return (
         <main className="tickets">
-            <div className="main-filter">
-            <label htmlFor="most-cheap" className="main-filter__label">
-                <input id="most-cheap" type="radio" name="most" value="cheap" defaultChecked />
-                Самый дешёвый
-            </label>
-            <label htmlFor="most-quick" className="main-filter__label">
-                <input id="most-quick" type="radio" name="most" value="quick" />
-                Самый быстрый
-            </label>
+            <div className="tickets__filter">
+                <input className="tickets__filter-radio" id="most-cheap" type="radio" name="most" value="cheap" defaultChecked />
+                <label htmlFor="most-cheap" className="tickets__filter-label">
+                    Самый дешёвый
+                </label>
+                <input className="tickets__filter-radio" id="most-quick" type="radio" name="most" value="quick" />
+                <label htmlFor="most-quick" className="tickets__filter-label">
+                    Самый быстрый
+                </label>
             </div>
             <ul className="tickets__list">
                 <Ticket />
