f46173cf336fcd30c2015aa2e7b205c85f308f2a
Roman Prudnikov <stillerr@ya.ru>
2019-09-19 01:24:30 +0300

    Fixed layovers height, added filter hover, removed some garbage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import Layovers from './components/layovers/layovers';
 import Tickets from './components/tickets/tickets';
 import Header from './components/header/header';
--- src/components/layovers/layovers.tsx
+++ src/components/layovers/layovers.tsx
@@ -4,29 +4,31 @@ import './layovers.css';
 const Layovers: React.FC = () => {
     return (
         <aside className="layovers">
-            <h2 className="layovers__heading">
-                Количество пересадок
-            </h2>
-            <ul className="layovers__list">
-                <li className="layovers__item">
-                    <input id="stops-all" type="checkbox" className="layovers__item-checkbox"/>
-                    <label htmlFor="stops-all" className="layovers__item-label">
-                        Все
-                    </label>
-                </li>
-                <li className="layovers__item">
-                    <input id="stops-0" type="checkbox" className="layovers__item-checkbox"/>
-                    <label htmlFor="stops-0" className="layovers__item-label">
-                        Без пересадок
-                    </label>
-                </li>
-                <li className="layovers__item">
-                    <input id="stops-1" type="checkbox" className="layovers__item-checkbox"/>
-                    <label htmlFor="stops-1" className="layovers__item-label">
-                        1 пересадка
-                    </label>
-                </li>
-            </ul>
+            <div className="layovers-inner">
+                <h2 className="layovers__heading">
+                    Количество пересадок
+                </h2>
+                <ul className="layovers__list">
+                    <li className="layovers__item">
+                        <input id="stops-all" type="checkbox" className="layovers__item-checkbox"/>
+                        <label htmlFor="stops-all" className="layovers__item-label">
+                            Все
+                        </label>
+                    </li>
+                    <li className="layovers__item">
+                        <input id="stops-0" type="checkbox" className="layovers__item-checkbox"/>
+                        <label htmlFor="stops-0" className="layovers__item-label">
+                            Без пересадок
+                        </label>
+                    </li>
+                    <li className="layovers__item">
+                        <input id="stops-1" type="checkbox" className="layovers__item-checkbox"/>
+                        <label htmlFor="stops-1" className="layovers__item-label">
+                            1 пересадка
+                        </label>
+                    </li>
+                </ul>
+            </div>
         </aside>
     );
 };
